298c775f95e6b380ff0725fa649ca87aabdb4945
facundo c <facucetera11@gmail.com>
2026-05-15 08:49:11 -0300

    Fix bug WhatsApp, loading state y description en modelo

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,37 +1,32 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const ProductSchema=new mongoose.Schema({
-
-name:{
-type:String,
-required:true
-},
-
-price:{
-type:Number,
-required:true,
-default:0
-},
-
-stock:{
-type:Number,
-required:true,
-default:0
-},
-
-image:{
-type:String,
-default:""
-},
-
-category:{
-type:String,
-required:true
-}
-
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  stock: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  image: {
+    type: String,
+    default: ""
+  },
+  description: {
+    type: String,
+    default: ""
+  },
+  category: {
+    type: String,
+    required: true
+  }
 });
 
-module.exports=mongoose.model(
-"Product",
-ProductSchema
-);
+module.exports = mongoose.model("Product", ProductSchema);
